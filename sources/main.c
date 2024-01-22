edf139dee263dc1c198bef0f2a063d0676f3437c
0Slaye <uduterrage@gmail.com>
2024-01-22 17:41:02 +0100

    Add: starting project

--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,23 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:41:22 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/01/22 16:58:10 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/01/22 17:39:45 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "imports.h"
 
-int main(void)
+int	pipex(void)
 {
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	if (argc != 5)
+		return (ft_putstr_fd("Error : wrong number of arguments.\n", 1), 0);
+	else
+		pipex();
 	return (0);
 }
