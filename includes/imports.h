e77fdeca9084c5b3e56fee576cd1aa59a80212f9
0Slaye <uduterrage@gmail.com>
2024-01-22 17:47:39 +0100

    Add: imports needed by subject

--- includes/imports.h
+++ includes/imports.h
@@ -6,15 +6,18 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:15:58 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/01/22 16:57:38 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/01/22 17:47:10 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IMPORTS_H
 # define IMPORTS_H
 
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include "../sources/libft/libft.h"
 
 #endif
